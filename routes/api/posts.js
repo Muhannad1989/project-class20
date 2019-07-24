04eb932a5bcd15cc51df4c74d8a505ba0666d684
unknown <muhannadjudah@gmail.com>
2019-07-24 11:15:19 +0200

    add route to unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -125,6 +125,35 @@ router.put('/like/:id', auth, async (request, response) => {
 
     response.json(post.likes);
   } catch (err) {
+    if (err.kind == 'ObjectId') {
+      return response.status(404).json({ msg: 'Post not found' });
+    }
+    console.error(err);
+    response.send('Server Error');
+  }
+});
+
+// @route           Update api/posts/unlike/:id
+// @description     Unlike post
+// @access          Private
+
+router.put('/unlike/:id', auth, async (request, response) => {
+  try {
+    // find the post which is related to this params id
+    const post = await Post.findById(request.params.id);
+
+    // get remove index
+    if (post.likes.filter(like => like.user.toString() === request.user.id).length === 0) {
+      return response.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+    const removeIndex = post.likes.map(like => like.user.id.toString()).indexOf(request.params.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    response.json(post.likes);
+  } catch (err) {
+    if (err.kind == 'ObjectId') {
+      return response.status(404).json({ msg: 'Post not found' });
+    }
     console.error(err);
     response.send('Server Error');
   }
