86f17c9104d000016fa5ec743bdb5834b528af10
unknown <muhannadjudah@gmail.com>
2019-07-24 10:32:13 +0200

    create route for adding like

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,4 +104,29 @@ router.delete('/:id', auth, async (request, response) => {
   }
 });
 
+// @route           Update api/posts/like/:id
+// @description     Add Like to post by ID
+// @access          Private
+
+router.put('/like/:id', auth, async (request, response) => {
+  try {
+    // find the post which is related to this params id
+    const post = await Post.findById(request.params.id);
+
+    // check if the post has already been liked
+    // go to post => likes
+    // match logged user with the same id as params
+    // if has the length bigger than 0 that means "is already exsist"
+    if (post.likes.filter(like => like.user.toString() === request.user.id).length > 0) {
+      return response.status(400).json({ msg: 'Post already liked' });
+    }
+    post.likes.unshift({ user: request.user.id });
+    await post.save();
+
+    response.json(post.likes);
+  } catch (err) {
+    console.error(err);
+    response.send('Server Error');
+  }
+});
 module.exports = router;
